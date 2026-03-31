fcea3896e26b63591ddc62cc4648873ece381ff6
Christine Clement <christineclem101@gmail.com>
2026-03-31 20:36:32 +0100

    Checking for different Tally formats

--- app.py
+++ app.py
@@ -17,8 +17,9 @@ def home():
 @app.route("/tally-webhook", methods=["GET", "POST"])
 def tally_webhook():
     if request.method == "POST":
-        data = request.json
-        print(data)
+        print("JSON:", request.json)
+        print("FORM:", request.form)
+        print("DATA:", request.data)
     return "", 200
 
 if __name__ == "__main__":
